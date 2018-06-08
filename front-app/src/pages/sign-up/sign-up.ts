eab8cdf10249bde2277a4753e3ccb5df825a8d17
YuraBoyco <boyco-yura@yandex.ru>
2018-06-09 02:32:05 +0300

    front-back work done

--- front-app/src/pages/sign-up/sign-up.ts
+++ front-app/src/pages/sign-up/sign-up.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import 'rxjs/add/operator/toPromise';
+import { HttpClient} from '@angular/common/http';
+import { Injectable} from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
 /**
  * Generated class for the SignUpPage page.
@@ -7,7 +11,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
-
+@Injectable()
 @IonicPage()
 @Component({
   selector: 'page-sign-up',
@@ -15,11 +19,31 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SignUpPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  articles: Observable<any>;
+
+  private apiUrl = 'http://localhost:8000/api/articles';
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private httpClient: HttpClient) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SignUpPage');
   }
 
+  getWhat(){
+
+  	console.log("function is working");
+  	this.articles = this.httpClient.get(this.apiUrl);
+  	this.articles
+    .subscribe(data => {
+      console.log('Articles: ', data);
+    });
+//   	console.log(this.httpClient.get(this.apiUrl,{headers: headers}).toPromise()
+// .then(response => response.json().data));
+  }
+
+ //  	getArticles(){
+  		
+ //  	
+	// }
 }
