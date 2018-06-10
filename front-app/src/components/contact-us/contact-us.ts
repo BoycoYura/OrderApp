80ac98be9a4ba37217d2466fb5de5a7c01975608
YuraBoyco <boyco-yura@yandex.ru>
2018-06-10 15:41:59 +0300

    Add update articles action

--- front-app/src/components/contact-us/contact-us.ts
+++ front-app/src/components/contact-us/contact-us.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ViewController } from 'ionic-angular';
+import { HttpClient} from '@angular/common/http';
 /**
  * Generated class for the ContactUsComponent component.
  *
@@ -14,9 +15,25 @@ export class ContactUsComponent {
 
   text: string;
 
-  constructor(public viewCtrl: ViewController) {
+  data = {
+    "title": "New article 2",
+    "body": "Article body 2"
+  };
+
+  constructor(public viewCtrl: ViewController, private httpClient: HttpClient) {
     console.log('Hello ContactUsComponent Component');
     this.text = 'Hello World';
+  }
+
+  updateArt(){
+    this.httpClient.put('http://localhost:8000/api/articles/4',this.data).subscribe(
+            res => {
+              alert(res);
+            },
+            err => {
+              console.log("Error occured");
+            });
+     console.log('User update');
   }
 
   toback() {
